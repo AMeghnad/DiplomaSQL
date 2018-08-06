9ea9d3a7abf52b0c952f0caabbacfd3acf3b73e3
AMeghnad <anshumanmeghnad@gmail.com>
2018-08-06 11:48:26 +1000

    Login changes and UI elements

---
+++ Assets/Scripts/GUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUI : MonoBehaviour {
+
+	public float scrW, scrH;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	
+	void OnGUI()
+	{
+		scrW = Screen.width / 16;
+		scrH = Screen.height / 9;
+		//GUI.Box(new Rect(7 * scrW, 2 * scrH, scrW, scrH), "");
+	}
+}
