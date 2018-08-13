10168534e01ae142f6b2daa45805980aebe01c50
AMeghnad <anshumanmeghnad@gmail.com>
2018-08-13 13:06:52 +1000

    Login screen changes

--- Assets/Scripts/LoginSystem.cs
+++ Assets/Scripts/LoginSystem.cs
@@ -1,17 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoginSystem : MonoBehaviour
 {
+    public bool isLogin = true;
     public string username, email, password;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
+        if(isLogin)
         {
-            Debug.Log("Sending...");
-            StartCoroutine(CreateAccount(username, email, password));
+
         }
     }
 
@@ -23,6 +24,35 @@ public class LoginSystem : MonoBehaviour
         user.AddField("email_Post", email);
         user.AddField("password_Post", password);
         WWW www = new WWW(createUserURL, user);
+        yield return www;
+        Debug.Log(www.text);
+    }
+  
+    public void SwapSets()
+    {
+        isLogin = !isLogin;
+    }
+
+    
+    public void Register()
+    {
+        Debug.Log("Sending...");
+        StartCoroutine(CreateAccount(username, email, password));
+    }
+
+    public void Login()
+    {
+        Debug.Log("Logging in...");
+        StartCoroutine(Login(username, password));
+    }
+
+    IEnumerator Login(string username, string password)
+    {
+        string loginURL = "http://localhost/sqlsystem/login.php";
+        WWWForm user = new WWWForm();
+        user.AddField("username_Post", username);
+        user.AddField("password_Post", password);
+        WWW www = new WWW(loginURL, user);
         yield return www;
         Debug.Log(www.text);
     }
